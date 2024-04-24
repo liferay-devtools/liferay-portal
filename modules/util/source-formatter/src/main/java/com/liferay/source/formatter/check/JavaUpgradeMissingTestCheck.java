30297a6ac9e657e50dcabdb62f89eafd0030fb2e
Alan Huang <alan.huang@liferay.com>
2024-04-24 20:05:17 +0800

    LPD-23646 Add JavaUpgradeMissingTestCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JavaUpgradeMissingTestCheck.java
@@ -0,0 +1,145 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.source.formatter.check;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.source.formatter.check.util.BNDSourceUtil;
+import com.liferay.source.formatter.check.util.JavaSourceUtil;
+import com.liferay.source.formatter.check.util.SourceUtil;
+import com.liferay.source.formatter.util.FileUtil;
+
+import java.io.File;
+
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Alan Huang
+ */
+public class JavaUpgradeMissingTestCheck extends BaseFileCheck {
+
+	@Override
+	public boolean isLiferaySourceCheck() {
+		return true;
+	}
+
+	@Override
+	protected String doProcess(
+		String fileName, String absolutePath, String content) {
+
+		String className = JavaSourceUtil.getClassName(fileName);
+
+		if (!absolutePath.contains("/upgrade/") ||
+			absolutePath.contains("-test/") || className.startsWith("Base") ||
+			!_isUpgradeProcess(absolutePath, content)) {
+
+			return content;
+		}
+
+		String expectedTestClassName = null;
+		File file = null;
+
+		if (isModulesFile(absolutePath)) {
+			expectedTestClassName = StringBundler.concat(
+				JavaSourceUtil.getPackageName(content), ".test.", className,
+				"Test");
+
+			file = JavaSourceUtil.getJavaFile(
+				expectedTestClassName, SourceUtil.getRootDirName(absolutePath),
+				_getBundleSymbolicNamesMap(absolutePath));
+		}
+		else if (absolutePath.contains("/portal-impl/") ||
+				 absolutePath.contains("/portal-kernel/")) {
+
+			expectedTestClassName = StringUtil.replaceFirst(
+				absolutePath,
+				new String[] {"/portal-impl/src/", "/portal-kernel/src/"},
+				new String[] {
+					"/portal-impl/test/unit/", "/portal-kernel/test/unit/"
+				});
+
+			expectedTestClassName = StringUtil.insert(
+				expectedTestClassName, "Test",
+				expectedTestClassName.length() - 5);
+
+			file = new File(expectedTestClassName);
+		}
+
+		if ((file == null) || !file.exists()) {
+			addMessage(
+				fileName,
+				"Test class '" + expectedTestClassName + "' does not exist");
+		}
+
+		return content;
+	}
+
+	private synchronized Map<String, String> _getBundleSymbolicNamesMap(
+		String absolutePath) {
+
+		if (_bundleSymbolicNamesMap != null) {
+			return _bundleSymbolicNamesMap;
+		}
+
+		_bundleSymbolicNamesMap = BNDSourceUtil.getBundleSymbolicNamesMap(
+			SourceUtil.getRootDirName(absolutePath));
+
+		return _bundleSymbolicNamesMap;
+	}
+
+	private boolean _isUpgradeProcess(String absolutePath, String content) {
+		String className = JavaSourceUtil.getClassName(absolutePath);
+
+		Pattern pattern = Pattern.compile(
+			" class " + className + "\\s+extends\\s+([\\w.]+) ");
+
+		Matcher matcher = pattern.matcher(content);
+
+		if (!matcher.find()) {
+			return false;
+		}
+
+		String extendedClassName = matcher.group(1);
+
+		if (extendedClassName.equals("UpgradeProcess")) {
+			return true;
+		}
+
+		pattern = Pattern.compile("\nimport (.*\\." + extendedClassName + ");");
+
+		matcher = pattern.matcher(content);
+
+		if (matcher.find()) {
+			extendedClassName = matcher.group(1);
+		}
+
+		if (!extendedClassName.contains(StringPool.PERIOD)) {
+			extendedClassName =
+				JavaSourceUtil.getPackageName(content) + StringPool.PERIOD +
+					extendedClassName;
+		}
+
+		if (!extendedClassName.startsWith("com.liferay.")) {
+			return false;
+		}
+
+		File file = JavaSourceUtil.getJavaFile(
+			extendedClassName, SourceUtil.getRootDirName(absolutePath),
+			_getBundleSymbolicNamesMap(absolutePath));
+
+		if (file == null) {
+			return false;
+		}
+
+		return _isUpgradeProcess(file.getAbsolutePath(), FileUtil.read(file));
+	}
+
+	private Map<String, String> _bundleSymbolicNamesMap;
+
+}
