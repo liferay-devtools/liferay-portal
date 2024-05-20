a724d6a10b6274cb5f35f1d1b24dca9f16b71383
Alan Huang <alan.huang@liferay.com>
2024-05-21 01:20:52 +0800

    LPD-25819 Enforce space around = in *.ftl

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/FTLTagCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/FTLTagCheck.java
@@ -93,12 +93,26 @@ public class FTLTagCheck extends BaseFileCheck {
 			StringBundler sb = new StringBundler(
 				(attributesMap.size() * 4) + 4);
 
-			sb.append(_getTagName(match));
+			String tagName = _getTagName(match);
+
+			sb.append(tagName);
+
 			sb.append(delimeter);
 
 			for (Map.Entry<String, String> entry : attributesMap.entrySet()) {
 				sb.append(entry.getKey());
-				sb.append(StringPool.EQUAL);
+
+				if (tagName.contains(".") ||
+					(tagName.contains("[") && tagName.contains("]"))) {
+
+					sb.append(StringPool.EQUAL);
+				}
+				else {
+					sb.append(StringPool.SPACE);
+					sb.append(StringPool.EQUAL);
+					sb.append(StringPool.SPACE);
+				}
+
 				sb.append(entry.getValue());
 				sb.append(delimeter);
 			}
