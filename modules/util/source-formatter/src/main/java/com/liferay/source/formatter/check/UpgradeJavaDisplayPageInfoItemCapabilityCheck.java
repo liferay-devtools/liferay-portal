b1bf2d3b7bcda8476915ba1354f38c20b5beec9d
Kyle Miho <kyle.miho@liferay.com>
2024-07-08 17:40:55 -0700

    LPD-30941 Add upgrade automation for upgrading reference to DisplayPageInfoItemCapability

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/UpgradeJavaDisplayPageInfoItemCapabilityCheck.java
@@ -0,0 +1,65 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.source.formatter.check;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Kyle Miho
+ */
+public class UpgradeJavaDisplayPageInfoItemCapabilityCheck
+	extends BaseUpgradeCheck {
+
+	@Override
+	protected String format(
+			String fileName, String absolutePath, String content)
+		throws Exception {
+
+		Matcher matcher = _pattern.matcher(content);
+
+		if (!matcher.find()) {
+			return content;
+		}
+
+		String annotationContent = _joinLines(
+			"@Reference(",
+			String.format(
+				"\t\ttarget = \"(info.item.capability.key=\" + " +
+					"DisplayPageInfoItemCapability.KEY + \")\""),
+			"\t)", "\tprivate InfoItemCapability " + matcher.group(1) + ";");
+
+		return matcher.replaceAll(annotationContent);
+	}
+
+	@Override
+	protected String[] getNewImports() {
+		return new String[] {
+			"com.liferay.info.item.capability.InfoItemCapability"
+		};
+	}
+
+	private String _joinLines(String... lines) {
+		StringBundler sb = new StringBundler((lines.length * 2) - 1);
+
+		for (String line : lines) {
+			if (sb.index() > 0) {
+				sb.append(StringPool.NEW_LINE);
+			}
+
+			sb.append(line);
+		}
+
+		return sb.toString();
+	}
+
+	private static final Pattern _pattern = Pattern.compile(
+		"@Reference\\s*private DisplayPageInfoItemCapability (\\w+);");
+
+}
