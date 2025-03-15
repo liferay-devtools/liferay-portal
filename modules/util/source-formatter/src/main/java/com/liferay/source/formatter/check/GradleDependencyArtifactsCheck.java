0b5548600f89f87c71253b2f54a2357f4fdafaa4
Alan Huang <alan.huang@liferay.com>
2025-03-15 09:26:39 +0800

    LPD-50777 SF

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/GradleDependencyArtifactsCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/GradleDependencyArtifactsCheck.java
@@ -233,9 +233,9 @@ public class GradleDependencyArtifactsCheck extends BaseFileCheck {
 					getLineNumber(content, pos));
 			}
 			else if (name.startsWith("com.liferay") &&
+					 !name.startsWith("com.liferay.gradle") &&
 					 !name.startsWith("com.liferay.jakarta.portlet-api") &&
 					 !name.startsWith("com.liferay.portletmvc4spring") &&
-					 !name.startsWith("com.liferay.gradle") &&
 					 !_isMasterOnlyFile(absolutePath)) {
 
 				for (String enforceVersionArtifact : enforceVersionArtifacts) {
